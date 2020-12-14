c6fa521d608908732ee2875cb746c55310c68349
wouterKenis1 <kenis.wouter@gmail.com>
2020-12-14 14:41:58 +0100

    13:10 merge with kevin

--- src/main/java/be/vdab/model/Book.java
+++ src/main/java/be/vdab/model/Book.java
@@ -1,8 +1,17 @@
 package be.vdab.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Book extends Article{
+
+
+
+    String author;
+    String isbn;
+    int pageCount;
 }
