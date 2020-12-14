058a68dd0ab0a71c2dd53f75f2c3719c31e44ae7
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-12-14 11:27:37 +0100

    DTO added

--- src/main/java/be/vdab/dto/ArticleDto.java
+++ src/main/java/be/vdab/dto/ArticleDto.java
@@ -1,10 +1,15 @@
 package be.vdab.dto;
 
+import be.vdab.model.Article;
+
 public class ArticleDto {
+    private Article article;
 
+    public Article getArticle() {
+        return article;
+    }
 
-
-
-
-
+    public void setArticle(Article article) {
+        this.article = article;
+    }
 }
