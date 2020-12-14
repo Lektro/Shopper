68e60c30737a052cc6b9db92e91048048dc0cbf8
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-12-14 11:25:20 +0100

    angular front end added

--- src/main/java/be/vdab/restcontroller/ArticleRestController.java
+++ src/main/java/be/vdab/restcontroller/ArticleRestController.java
@@ -1,4 +1,22 @@
 package be.vdab.restcontroller;
 
+import be.vdab.model.Article;
+import be.vdab.service.ArticleService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+
+@RestController
+@RequestMapping("/article/")
 public class ArticleRestController {
+    private ArticleService articleService;
+
+    public ArticleRestController (ArticleService articleService) { this.articleService = articleService; }
+
+    @GetMapping(value = { "", "/" })
+    public @NotNull Iterable<Article> getArticles() {
+        return articleService.getAllArticles();
+    }
 }
