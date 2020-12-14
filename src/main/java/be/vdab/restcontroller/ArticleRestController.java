71625893080e718208a26c2372e4c884278b20ea
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-12-14 14:45:43 +0100

    routing to port 8080 not working, qrticle renders on port 4200

--- src/main/java/be/vdab/restcontroller/ArticleRestController.java
+++ src/main/java/be/vdab/restcontroller/ArticleRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.constraints.NotNull;
 
 @RestController
-@RequestMapping("/article/")
+@RequestMapping("/api/article/")
 public class ArticleRestController {
     private ArticleService articleService;
 
